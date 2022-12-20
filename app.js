14dbb7dc7d905d4cdc2d482e9b386b4791c65014
Haksoo Ji <mintzzz1009@gmail.com>
2022-12-20 18:04:58 +0900

    added Dir: ./assets to rendering front-page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const app = express();
-const router = express.Router();
+const todosRouter = require("./routes/todos.router.js");
 
-router.get("/", (req, res) => {
-  res.send("Hi!");
-});
-app.use("/api", express.json(), router);
+const app = express();
+
+app.use("/api", express.json(), todosRouter);
+app.use(express.static("./assets"));
+// express.static 정적인 파일을 연결해주는 미들웨어
+// 특정 주소로 들어왔을 때, 그 주소가 ./assets 경로 안에 있다면 그 파일로 연결해줘라
 
 app.listen(8080, () => {
   console.log("서버가 켜졌어요!");
---
+++ routes/todos.router.js
@@ -0,0 +1,10 @@
+// routes/todos.router.js
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hi!");
+});
+
+module.exports = router;
